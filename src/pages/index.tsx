820fb45622478897df9b14cf022378f73038f88d
Fredrik Wendel <fredrik.wendel@cygni.se>
2023-04-25 21:51:44 +0200

    chore: changed font for text

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,23 @@
 import Image from 'next/image'
-import {Roboto_Flex} from 'next/font/google'
+import {Libre_Baskerville} from 'next/font/google'
 import {SparklesIcon} from '@heroicons/react/24/outline';
 
 import TailwindLogo from '@/tailwindcss-logotype.svg'
+import clsx from 'clsx';
 
-const inter = Roboto_Flex({subsets: ['latin']})
+const googleFont = Libre_Baskerville({subsets: ['latin'], weight: '400', variable: '--font-roboto'})
 
 export default function Home() {
   return (
-    <main className="flex items-center justify-center bg-white h-full">
+    <main className='flex items-center justify-center bg-white h-full'>
       <article className="h-full w-[500px] mt-96 flex flex-col gap-4 bg-pink-50 p-8 -pl-8 rounded-3xl">
         <div className="bg-pink-500 rounded-xl p-4 flex flex-row self-end w-fit text-white gap-4">
-          <SparklesIcon width={24} /> Achievement unlocked
+          <SparklesIcon width={24}/> Achievement unlocked
         </div>
 
-        <div className='flex flex-col items-end w-fit'>
+        <div className="flex flex-col items-end w-fit">
           <Image src={TailwindLogo} alt="Tailwind logo" height={50}/>
-          <h3 className="font-bold text-xl">Tailwind Apprentice, baby steps</h3>
+          <h3 className={clsx('font-bold text-xl', googleFont.className)}>Tailwind Apprentice, baby steps</h3>
         </div>
       </article>
     </main>
